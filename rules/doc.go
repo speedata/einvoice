bee213877751cbf5e92f35162c971b720efbd3fa
Florian Kinder <florian.kinder@enthus.de>
2025-10-07 23:18:02 +0900

    Further simplify rules/doc.go
    Minimal godoc - just package description and pointer to detailed docs.

--- rules/doc.go
+++ rules/doc.go
@@ -1,23 +1,5 @@
-// Package rules contains EN 16931 business rule definitions for electronic invoicing validation.
+// Package rules contains EN 16931 business rule definitions.
 //
-// Rules are auto-generated from official schematron specifications maintained by CEN/TC 434.
-// Source: https://github.com/ConnectingEurope/eInvoicing-EN16931
-//
-// # Generation
-//
-// Rules are regenerated using go generate:
-//
-//	cd rules && go generate
-//
-// See cmd/genrules/README.md for detailed generation instructions.
-//
-// # Usage
-//
-//	import "github.com/speedata/einvoice/rules"
-//
-//	func (inv *Invoice) validate() {
-//	    if inv.SpecificationIdentifier == "" {
-//	        inv.addViolation(rules.BR1, "Missing specification identifier")
-//	    }
-//	}
+// Rules are auto-generated from official schematron specifications.
+// See cmd/genrules/README.md for generation instructions.
 package rules
